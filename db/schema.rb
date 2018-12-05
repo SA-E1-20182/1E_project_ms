783797982e957d3ff08666eccd5f53433d212e78
unknown <rcbuelvasv@gmail.com>
2018-12-05 16:44:38 -0500

    It works, I swear

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_031053) do
+ActiveRecord::Schema.define(version: 2018_11_08_025938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_10_04_031053) do
     t.string "author_id"
     t.string "current_version_id"
     t.datetime "updated_at", null: false
+    t.string "cover_url"
   end
 
 end
