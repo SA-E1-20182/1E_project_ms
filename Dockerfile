bd77fbda032ffd2c17b8ad0fedd120bbaadc168c
fffeelipe <lfepiar@unal.edu.co>
2018-12-05 22:54:47 -0500

    It looks like it really works =D

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,3 @@ COPY . /myapp
 
 RUN chmod +x entrypoint.sh
 CMD "./entrypoint.sh"
-CMD ["bundle", "exec", "rails", "server", "-b", "0.0.0.0"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 echo "creating and migrating database"
 sleep 10
 rails db:create
+echo "creating and migrating databasedfsdfsdfsdfsdfsd"
 rails db:migrate
+echo "creating and migrating databaseslfkjgseirjgñsdfkjgñsdigjñ"
+
 rails s
